6094c142e479057384a49548a29dfd0c2286d9d0
ridha fathima <shamilshamilind@gmail.com>
2026-09-19 08:46:34 +0530

    Created login and register pages

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -7,25 +7,27 @@ function Register(){
     const handleregister=(e)=>{
         e.preventDefault();
         console.log(name);
-        console.log
+        console.log(email)
+        console.log(password)
     }
     return(
     <div>
+        <h1>Glowora</h1>
         <h1>
             Create Account
         </h1>
-        <form>
+        <form onSubmit={handleregister}>
             <div>
                 <label>name</label>
-                <input type="text" placeholder="enter your name"/>
+                <input type="text" placeholder="enter your name" onChange={(e)=>setname(e.target.value)}/>
             </div>
             <div>
                 <label>email</label>
-                <input type="email" placeholder="enter your email"/>
+                <input type="email" placeholder="enter your email" onChange={(e)=>setemail(e.target.value)}/>
             </div>
             <div>
                 <label>password</label>
-                <input type="password" placeholder="create a password"/>
+                <input type="password" placeholder="create a password" onChange={(e)=>setpassword(e.target.value)}/>
             </div>
             <button type="submit">Register</button>
             <p>already have an account<Link to="/login">login</Link></p>
