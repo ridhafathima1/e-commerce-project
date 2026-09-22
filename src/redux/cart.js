5a3a6c004b8fd6e6a098d4fee6e6663c84375c94
ridha fathima <shamilshamilind@gmail.com>
2026-09-22 11:07:30 +0530

    Created Redux cart slice

---
+++ src/redux/cart.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit"
+const cartslice=createSlice({
+    name:"cart",
+    initialState:{
+        items:[],
+    },
+    reducers:{
+        addtocart:(state,action)=>{
+            const exists=state.items.find((items)=>items.id===action.payload.id)
+        
+        if(!exists){
+            state.items.push(action.payload);
+        }
+    },
+    removefromcart:(state,action)=>{
+        state.items=state.items.filter((item)=>item.id!==action.payload)
+    },
+    },
+}),
+export const { addtocart,removefromcart }=cartslice.actions
+export default cartslice.reducer;
